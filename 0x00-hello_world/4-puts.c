8f344bf245cfd7d65fb69e832f5c3649360a0223
OKT-17 <roritabane@gmail.com>
2023-02-20 13:38:33 -0800

    C is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succces)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multinational puzzle
+");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with the proper grammar, but the outcome is piece of art,\n");
+	return(0);
+}
